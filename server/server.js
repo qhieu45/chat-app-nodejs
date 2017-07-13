438e53c66e20bfad408d6cc7e676815c565b9859
Hieu Cao <qhieu45@gmail.com>
2017-07-13 13:47:44 +0300

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -14,9 +14,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected!');
 
+    socket.on('createMessage', (message) => {
+        console.log(`New message created: `, message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        })
+    });
+
     socket.on('disconnect', () => {
         console.log(`Client disconnected!`);
-    })
+    });
 })
 
 server.listen(port, () => {
