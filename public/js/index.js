55fc7235397e8dad93debeaa6812e74cf3b07b7b
Hieu Cao <qhieu45@gmail.com>
2017-07-13 15:13:20 +0300

    Add form for messages and show messages

--- public/js/index.js
+++ public/js/index.js
@@ -11,4 +11,24 @@ socket.on('disconnect', () => {
 socket.on('newMessage', (message) => {
     console.log(`New message incoming: `)
     console.log(message);
+    let li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    jQuery('#messages').append(li);
+})
+
+// socket.emit('createMessage', {
+//     from: 'Frank',
+//     text: 'Hi'
+// }, (data) => {
+//     console.log('Got it!! ', data);
+// })
+
+jQuery('#message-form').on('submit', (e) => {
+    e.preventDefault();
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, () => {
+
+    })
 })
