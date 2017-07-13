3822e7db3b5cd8c8c93b5040515ec524ce77b219
Hieu Cao <qhieu45@gmail.com>
2017-07-13 14:11:51 +0300

    Greet new user, and alert others

---
+++ public/js/index.js
@@ -0,0 +1,14 @@
+const socket = io();
+
+socket.on('connect', () => {
+    console.log(`Connected to server`);
+})
+
+socket.on('disconnect', () => {
+    console.log(`Disconnected from server`);
+})
+
+socket.on('newMessage', (message) => {
+    console.log(`New message incoming: `)
+    console.log(message);
+})
